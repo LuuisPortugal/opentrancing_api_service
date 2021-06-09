4b183d14b74a9337e0c7e0a4bb4255ea0f293b2b
Luis Portugal <luisenriquegomesportugal@gmail.com>
2021-06-09 12:49:00 -0300

    Hello World

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_service.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public String hello()
+        {
+            return "Hello World";
+        }
+    }
+}
